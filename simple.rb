a0f06227037199851ae68301ab6e4a4583c7498c
Wei Ang <weiang123@gmail.com>
2015-03-16 18:14:17 +0800

    Statement class is added.

--- simple.rb
+++ simple.rb
@@ -12,6 +12,48 @@ class Number < Struct.new(:value)
 	end
 end
 
+class Boolean < Struct.new(:value)
+	def to_s
+		value.to_s
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def reducible?
+		return false
+	end
+end
+
+class And < Struct.new(:left, :right)
+	def to_s
+		"#{left} & #{right}"
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def reducible?
+		return true
+	end
+
+	def reduce(environment)
+		if left.reducible?
+			And.new(left.reduce(environment), right)
+		elsif right.reducible?
+			And.new(left, right.reduce(environment))
+		else
+			if left.value and right.value 
+				Boolean.new(true)
+			else
+				Boolean.new(false)
+			end
+		end
+	end
+end
+
 class Add < Struct.new(:left, :right)
 	def to_s
 		"#{left} + #{right}"
@@ -25,11 +67,11 @@ class Add < Struct.new(:left, :right)
 		return true
 	end
 
-	def reduce
+	def reduce(environment)
 		if left.reducible?
-			Add.new(left.reduce, right)
+			Add.new(left.reduce(environment), right)
 		elsif right.reducible?
-			Add.new(left, right.reduce)
+			Add.new(left, right.reduce(environment))
 		else
 			Number.new(left.value + right.value)
 		end
@@ -49,14 +91,111 @@ class Multiply < Struct.new(:left, :right)
 		return true
 	end
 
-	def reduce
+	def reduce(environment)
 		if left.reducible?
-			Multiply.new(left.reduce, right)
+			Multiply.new(left.reduce(environment), right)
 		elsif right.reducible?
-			Multiply.new(left, right.reduce)
+			Multiply.new(left, right.reduce(environment))
 		else
 			Number.new(left.value * right.value)
 		end
+	end
+end
+
+
+class LessThan < Struct.new(:left, :right)
+	def to_s
+		"#{left} < #{right}"
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def reducible?
+		true
+	end
+
+	def reduce(environment)
+		if left.reducible?
+			LessThan.new(left.reduce(environment), right)
+		elsif right.reducible?
+			LessThan.new(left, right.reduce(environment))
+		else
+			Boolean.new(left.value < right.value)
+		end
+	end
+end
+
+class Variable < Struct.new(:name)
+	def to_s
+		name.to_s
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def reducible?
+		true
+	end
+
+	def reduce(environment)
+		environment[name]
+	end
+end
+
+class DoNothing
+	def to_s
+		"do-nothing"
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def ==(other_statement)
+		other_statement.instance_of?(DoNothing)
+	end
+
+	def reducible?
+		false
+	end
+end
+
+class Asign < Struct.new(:name, :expression)
+	def to_s
+		"#{name} = #{expression}"
+	end
+
+	def inspect
+		"<<#{self}>>"
+	end
+
+	def reducible?
+		true
+	end
+
+	def reduce(environment)
+		if expression.reducible?
+			[Asign.new(name, expression.reduce(environment)), environment]
+		else
+			[DoNothing.new, environment.merge({name => expression})]
+		end
+	end
+end
+			
+class Machine < Struct.new(:statement, :environment)
+	def step
+		self.statement, self.environment = statement.reduce(environment)
+	end
+
+	def run
+		while statement.reducible?
+			puts "#{statement}, #{environment}"
+			step
+		end
+		puts "#{statement}, #{environment}"
 	end
 end
 
@@ -64,3 +203,9 @@ expression = Add.new(
 		Multiply.new(Number.new(1), Number.new(2)),
 		Multiply.new(Number.new(3), Number.new(4))
 	)
+
+Machine.new(
+	Asign.new(:x, Add.new(Variable.new(:x), Number.new(1))), 
+	{x: Number.new(2)}
+).run
+
